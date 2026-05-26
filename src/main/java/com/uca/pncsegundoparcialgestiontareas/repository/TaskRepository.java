302c329f5c00d304aa85f853e4ef3eb0805d35ab
Andrea-004 <00106123@uca.edu.sv>
2026-05-25 20:03:31 -0600

    adding service base operations

--- src/main/java/com/uca/pncsegundoparcialgestiontareas/repository/TaskRepository.java
+++ src/main/java/com/uca/pncsegundoparcialgestiontareas/repository/TaskRepository.java
@@ -3,6 +3,10 @@ package com.uca.pncsegundoparcialgestiontareas.repository;
 import com.uca.pncsegundoparcialgestiontareas.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
         boolean existsByTitle(String title);
+        List<Task> findByStatus(String status);
+        List<Task> findByPriority(String priority);
 }
---
+++ src/main/java/com/uca/pncsegundoparcialgestiontareas/service/TaskService.java
@@ -0,0 +1,62 @@
+package com.uca.pncsegundoparcialgestiontareas.service;
+import com.uca.pncsegundoparcialgestiontareas.dto.request.TaskDTORequest;
+import com.uca.pncsegundoparcialgestiontareas.dto.response.TaskDTOResponse;
+import com.uca.pncsegundoparcialgestiontareas.entities.Task;
+import com.uca.pncsegundoparcialgestiontareas.repository.TaskRepository;
+import com.uca.pncsegundoparcialgestiontareas.utils.enums.Status;
+import com.uca.pncsegundoparcialgestiontareas.utils.mappers.TaskMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@AllArgsConstructor
+public class TaskService {
+    private final TaskRepository taskRepository;
+
+    //Crear una nueva tarea
+    public void createTask(TaskDTORequest task){
+        if (taskRepository.existsByTitle(task.title().toLowerCase())){
+            throw new IllegalArgumentException("Task with title " + task.title() + " already exists");
+        }
+        taskRepository.save(TaskMapper.toEntity(task));
+    }
+
+    //Listar todas las tareas (con filtros opcionales)
+    public List<TaskDTOResponse> findAllTasks(){
+        return taskRepository.findAll().stream()
+                .map(TaskMapper::toResponse)
+                .toList();
+    }
+
+    //Obtener una tarea por ID
+    public TaskDTOResponse findTaskById(Long id){
+        return TaskMapper.toResponse(taskRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Task not found with id " + id)
+        ));
+    }
+
+    //Actualizar la información de una tarea
+    public void updateTask(Long id, TaskDTORequest task){
+        Task taskToUpdate = TaskMapper.toEntity(task);
+        if (taskRepository.existsById(id)){
+            taskToUpdate.setId(id);
+        }else{
+            throw new IllegalArgumentException("Task not found with id " + id);
+        }
+        taskRepository.save(taskToUpdate);
+    }
+
+    //Eliminar una tarea (respetando reglas de negocio)
+    public void deleteTaskById(Long id){
+        Task taskToDelete = taskRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("Task not found with id " + id)
+        );
+        if (taskToDelete.getStatus().equals(Status.IN_PROGRESS) || taskToDelete.getStatus().equals(Status.REVIEW)){
+            throw new IllegalStateException("Cannot delete tasks with IN_PROGRESS or REVIEW status");
+        }
+        taskRepository.deleteById(id);
+    }
+}
